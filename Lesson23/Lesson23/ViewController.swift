9ebf7243d51e2f4813f8fad6b3d0771ea1fedf8d
admin <admin@MacBook-Air-admin.local>
2022-04-12 20:27:59 +0300

    dobavlen XCode project

---
+++ Lesson23/Lesson23/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson23
+//
+//  Created by admin on 12.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
